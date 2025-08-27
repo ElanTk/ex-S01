9a13959fdecee6292b146e245b0c1ed9a100a606
Luís <90935726+ElanTk@users.noreply.github.com>
2025-08-27 20:15:49 -0300

    Add files via upload

---
+++ ex1-multiplos.lua
@@ -0,0 +1,13 @@
+print("Escreve o M:")
+    m = io.read("*number")      
+    print("Escreve o N:")
+    n = io.read("*number")      
+    print("Por ultimo, escreva o X:")
+    x = io.read("*number")   
+    
+    for i = m, n, 1
+    do
+      if i % x == 0 then
+        print(i)
+      end
+    end
